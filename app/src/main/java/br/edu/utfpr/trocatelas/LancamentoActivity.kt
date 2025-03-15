7738f4a14c5054062f10fceca04e1c3b0ff90927
Willane Paiva <willanepaiva@hotmail.com>
2025-03-15 15:06:35 -0300

    aula 1

---
+++ app/src/main/java/br/edu/utfpr/trocatelas/LancamentoActivity.kt
@@ -0,0 +1,14 @@
+package br.edu.utfpr.trocatelas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LancamentoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lancamento)
+    }
+}
